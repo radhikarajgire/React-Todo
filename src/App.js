ecc9f22bfa9a88c897eae49652adbbb5306f3702
radhikarajgire <58374458+radhikarajgire@users.noreply.github.com>
2020-09-07 21:37:51 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import trashIcon from "./trash.svg";
 function App() {
   const [taskInput, updateTaskInput] = useState("");
   const [toDoList, updateToDoList] = useState([]);
+  const inputKeyDown= (event) => {
+    if (event.keyCode===13)
+    addNote();
+  }
+  
+
   const addNote = () => {
-    toDoList.push({ description: taskInput });
+    toDoList.push({ description: taskInput,isComplete: false });
     updateToDoList(toDoList);
     updateTaskInput("");
-  };
+  }
+  const deleteTask = (index) =>{
+    const newList = toDoList.filter((item,i) => i !== index);
+    updateToDoList(newList)
+  }
+
+  const markComplete = (index) => {
+    const list = [...toDoList];
+    list[index].isComplete =!list[index].isComplete ;
+    updateToDoList(list)
+  }
+
   return (
     <div className="app-background">
       <p className="heading-text">My To-do List </p>
@@ -17,6 +34,7 @@ function App() {
           <input
             className="text-input"
             value={taskInput}
+            onKeyDown={inputKeyDown}
             onChange={(e) => updateTaskInput(e.target.value)}
           />
           <button className="add-button" onClick={addNote}>
@@ -24,7 +42,7 @@ function App() {
           </button>
         </div>
 
-        {toDoList?.length ? (toDoList.map((toDoObject, index) => <ListItem index={index} itemData={toDoObject}/>) ) :
+        {toDoList?.length ? (toDoList.map((toDoObject, index) => <ListItem index={index} itemData={toDoObject} deleteTask={deleteTask} markComplete={markComplete}/>) ) :
          ( <p className="no-item-text"> No Task Added! </p> )}
       </div>
 
@@ -35,8 +53,8 @@ function App() {
 function ListItem(props) {
   return (
     <div className="list-item row jc-space-between">
-      <span>{props.itemData.description}</span>
-      <img src={trashIcon} className="delete-icon" />
+      <span className={props.itemData.isComplete?"task-complete":""} onClick ={()=>props.markComplete(props.index)}>{props.itemData.description}</span>
+      <img src={trashIcon} className="delete-icon" onClick={() => props.deleteTask(props.index)}/>
     </div>
   );
 }
