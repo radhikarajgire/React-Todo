a97342324d2a4fe57f55a767bf77e701d82df40b
radhikarajgire <58374458+radhikarajgire@users.noreply.github.com>
2020-11-03 19:48:26 +0100

    Updates

---
+++ precache-manifest.0ddb3c5bc4891f037298296dff7d758c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f47b4e2d64ef7948ddec908e94bec2",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "7c7e866da041cd86b39f",
+    "url": "/React-Todo/static/css/main.eb526c97.chunk.css"
+  },
+  {
+    "revision": "55b72a841c3590477e5b",
+    "url": "/React-Todo/static/js/2.a6fcdeeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo/static/js/2.a6fcdeeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7e866da041cd86b39f",
+    "url": "/React-Todo/static/js/main.ad0f5373.chunk.js"
+  },
+  {
+    "revision": "cd28791556030fe4d893",
+    "url": "/React-Todo/static/js/runtime-main.7ab11240.js"
+  },
+  {
+    "revision": "7832cf73e3be83f87350a45deeb0a08c",
+    "url": "/React-Todo/static/media/trash.7832cf73.svg"
+  }
+]);
